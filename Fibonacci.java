f6cfc16a16362bdb8bc01eb2ae47646fc59640d5
Anushavish18 <109326243+Anushavish18@users.noreply.github.com>
2022-07-15 09:30:22 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		int n = 11, firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci Series till " + n + " terms:");
+
+	    for (int i = 1; i <= n; ++i)
+	    {
+	      System.out.print(firstTerm + ", ");
+	      int nextTerm = firstTerm + secondTerm;
+	      firstTerm = secondTerm;
+	      secondTerm = nextTerm;
+        } 
+	}
+}
+	
+		
+		
+	
+
+
